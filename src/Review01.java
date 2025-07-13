3350181da91bde1477b69be53082ff7fb10243a7
mc-hsss <m.hashimoto03.work@gmail.com>
2025-07-14 00:06:05 +0900

    「Add Review01課題ファイル

---
+++ src/Review01.java
@@ -0,0 +1,17 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int price = 1500;
+        int tax = tax(price);
+        int total = price + tax;
+        System.out.println(price + "円の商品の税込価格は" + total + "円（消費税は" + tax + "円）です。");
+
+    }
+public static int tax(int price) {
+    int tax = (int)(price * 0.1);
+    return tax;
+
+    }
+
+}
